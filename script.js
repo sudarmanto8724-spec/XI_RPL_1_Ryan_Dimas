cd105a9893e9e91b195e0261b12ac5c6fb6deb54
Annisa213@gmail.com <Annisa213@gmail.com>
2026-09-07 15:11:17 +0700

    p

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function  tampilkanNama(){
+// === FUNGSI HALAMAN KONTAK ANGGOTA ===
+function tampilkanNama(){
     document.getElementById("namaAnggota").innerHTML = `
     <ol
         style="list-style-type: decimal;
@@ -12,3 +13,30 @@ function  tampilkanNama(){
        </button>
     `;
 }
+
+// === FUNGSI HALAMAN RESEP ===
+function tampilkanPopUp() {
+    document.getElementById("popupBox").style.display = "block";
+}
+
+function tutupPopUp() {
+    document.getElementById("popupBox").style.display = "none";
+}
+
+// Tutup pop-up jika klik di luar kotak
+window.onclick = function(event) {
+    const popup = document.getElementById("popupBox");
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
+
+// === FUNGSI HALAMAN SURAT IZIN ===
+function validasiFrom() {
+    var tglMulai = document.getElementById("tglMulai").value;
+    var tglSelesai = document.getElementById("tglSelesai").value;
+}
+    if(new Date(tglSelesai) < new Date(tglMulai)) {
+        alert('Tanggal Selesai Tidak Boleh Lebih Awal Lebih Dari Tanggal Mulai');
+    }
+    return true;
